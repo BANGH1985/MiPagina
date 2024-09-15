c01c43045ef4f6d357b61b5180253a21fa6aaf52
BANGH1985 <dantesoriano30@gmail.com>
2024-09-15 01:26:26 -0300

    Arreglo link de Github

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -6,7 +6,7 @@ const Contacto = () => (
     <h2>Contacto</h2>
     <p>Email: dantesoriano30@gmail.com</p>
     <p>Teléfono: 1144711978</p>
-    <p>Github: <a target='blank' href="https://github.com/dantesoriano30">Perfil de Github</a></p>
+    <p>Github: <a target='blank' href="https://github.com/BANGH1985">Perfil de Github</a></p>
     <p>LinkedIn: <a target='blank' href="https://www.linkedin.com/in/dantensoriano/">Perfil de LinkedIn</a></p>
     <p>Twitter: <a target='blank' href="https://x.com/banghi1tv">Perfil de Twitter</a></p>
     <p>Instagram: <a target='blank' href="https://www.instagram.com/banghi1tv/?hl=es-la">Perfil de Instagram</a></p>
